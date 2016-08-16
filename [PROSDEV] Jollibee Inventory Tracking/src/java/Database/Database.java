859e63fe2d1f48212c116d59faa912fa70fc068f
ebackers <arcestalavera@gmail.com>
2016-08-16 13:07:38 +0800

    Revert "Revert "update db + request stuff""
    This reverts commit 3413834712a3c37a222f5a36ea97fb301fd85ee2.

--- [PROSDEV] Jollibee Inventory Tracking/src/java/Database/Database.java
+++ [PROSDEV] Jollibee Inventory Tracking/src/java/Database/Database.java
@@ -875,13 +875,14 @@ public class Database {
      METHODS THAT WILL EDIT THE DETAILS OF AN OBJECT
      */
     public void respondRequest(int id, String response) {
-        sql = "UPDATE requests SET status = ?"
+        sql = "UPDATE requests R NATURAL JOIN item_of_request IR NATURAL JOIN deliveries D"
+                + " SET R.status = ?, D.status = ?"
                 + " WHERE requestID = ?";
-
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, response);
-            ps.setInt(2, id);
+            ps.setString(2, response);
+            ps.setInt(3, id);
 
             ps.execute();
         } catch (SQLException e) {
@@ -1074,6 +1075,7 @@ public class Database {
             //update item count in warehouse
             sql = "UPDATE place_of_item SET count = ?"
                     + " WHERE warehouseID = ? AND itemID = ?";
+            
             ps = con.prepareStatement(sql);
 
             ps.setInt(1, current + count);
